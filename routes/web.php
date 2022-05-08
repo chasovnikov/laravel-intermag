ccfb14b81a7a5c23fe2981b94fb9949826c7fcc1
Alexander Chasovnikov <a.s.chasovnikov@yandex.ru>
2022-05-08 14:53:58 +0300

    create MainController, migration: products and categories, ...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,70 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes([
+    'reset' => false,
+    'confirm' => false,
+    'verify' => false,
+]);
+
+Route::get('locale/{locale}', [MainController::class, 'changeLocale'])->name('locale');
+Route::get('currency/{currencyCode}', [MainController::class, 'changeCurrency'])->name('currency');
+
+Route::get('/logout', 'Auth\LoginController@logout')->name('get-logout');
+
+// Route::middleware(['set_locale'])->group(function () {
+Route::get('reset', 'ResetController@reset')->name('reset');
+
+Route::middleware(['auth'])->group(function () {
+    Route::group([
+        'prefix' => 'person',
+        'namespace' => 'Person',
+        'as' => 'person.',
+    ], function () {
+        Route::get('/orders', 'OrderController@index')->name('orders.index');
+        Route::get('/orders/{order}', 'OrderController@show')->name('orders.show');
+    });
+
+    Route::group([
+        'namespace' => 'Admin',
+        'prefix' => 'admin',
+    ], function () {
+        Route::group(['middleware' => 'is_admin'], function () {
+            Route::get('/orders', 'OrderController@index')->name('home');
+            Route::get('/orders/{order}', 'OrderController@show')->name('orders.show');
+        });
+
+        Route::resource('categories', 'CategoryController');
+        Route::resource('products', 'ProductController');
+        Route::resource('products/{product}/skus', 'SkuController');
+        Route::resource('properties', 'PropertyController');
+        Route::resource('merchants', 'MerchantController');
+        Route::get('merchant/{merchant}/update_token', 'MerchantController@updateToken')->name('merchants.update_token');
+        Route::resource('coupons', 'CouponController');
+        Route::resource('properties/{property}/property-options', 'PropertyOptionController');
+    });
 });
+
+Route::controller(MainController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/categories', 'categories')->name('categories');
+    Route::post('subscription/{skus}', 'subscribe')->name('subscription');
+    Route::get('/{category}', 'category')->name('category');
+    Route::get('/{category}/{product}/{skus}', 'sku')->name('sku');
+});
+
+
+Route::group(['prefix' => 'basket'], function () {
+    Route::post('/add/{skus}', 'BasketController@basketAdd')->name('basket-add');
+
+    Route::group([
+        'middleware' => 'basket_not_empty',
+    ], function () {
+        Route::get('/', 'BasketController@basket')->name('basket');
+        Route::get('/place', 'BasketController@basketPlace')->name('basket-place');
+        Route::post('/remove/{skus}', 'BasketController@basketRemove')->name('basket-remove');
+        Route::post('/place', 'BasketController@basketConfirm')->name('basket-confirm');
+        Route::post('coupon', 'BasketController@setCoupon')->name('set-coupon');
+    });
+});
+// });
